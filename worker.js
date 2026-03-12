78f6642a006bbf8cf5d9f4a1f874c05b2d2fbf8b
1642128 <jmwill321jack2@gmail.com>
2026-03-12 09:13:17 -0400

    Add WebWorkerMLCEngineHandler to worker.js

---
+++ worker.js
@@ -0,0 +1,5 @@
+// worker.js (module worker)
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
+
+const handler = new WebWorkerMLCEngineHandler();
+self.onmessage = (msg) => handler.onmessage(msg);
